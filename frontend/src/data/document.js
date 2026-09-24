e2ffd7358e055e80ddb5ceddfe17cbe3e4e32b0f
Adam Lemesanyi <alemesanyi@gmail.com>
2026-09-24 16:19:53 +0200

    fix: avoid duplicate document save error toasts

--- frontend/src/data/document.js
+++ frontend/src/data/document.js
@@ -73,7 +73,7 @@ export function useDocument(doctype, docname, resourceOverrides = {}) {
                 toast.error(msg)
               })
 
-              if (err.messages?.length === 0) {
+              if (!err.messages?.length) {
                 toast.error(__('An error occurred while updating the document'))
               }
 
