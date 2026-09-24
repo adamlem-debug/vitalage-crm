284c6f8e853964072bd4dd7ff3542b7dc61f3bdb
Adam Lemesanyi <alemesanyi@gmail.com>
2026-09-24 10:32:19 +0200

    feat: add note edit action in activities

--- frontend/src/components/Activities/NoteArea.vue
+++ frontend/src/components/Activities/NoteArea.vue
@@ -19,6 +19,11 @@
       <Dropdown
         :options="[
           {
+            label: __('Edit'),
+            icon: 'edit-2',
+            onClick: () => modalRef.showNote(note),
+          },
+          {
             label: __('Delete'),
             icon: 'trash-2',
             onClick: () => deleteNote(note.name),
@@ -67,6 +72,7 @@ import { usersStore } from '@/stores/users'
 
 defineProps({
   note: { type: Object, default: () => ({}) },
+  modalRef: { type: Object, default: () => ({}) },
 })
 
 const notes = defineModel({ type: Object })
