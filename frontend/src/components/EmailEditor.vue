50141ea9230d261805458646ccd3afe8542e1230
Adam Lemesanyi <alemesanyi@gmail.com>
2026-09-24 16:12:21 +0200

    fix: apply reply sender after email accounts load

--- frontend/src/components/EmailEditor.vue
+++ frontend/src/components/EmailEditor.vue
@@ -280,10 +280,16 @@ const from = computed(() => {
   return emails
 })
 
+const replyAddresses = ref([])
+
 watch(
-  from,
-  (fromOptions) => {
-    if (!fromOptions.find((f) => f.value === fromEmail.value)) {
+  [from, replyAddresses],
+  ([fromOptions, addresses]) => {
+    let match = addresses.find((a) => fromOptions.some((f) => f.value === a))
+    if (match) {
+      fromEmail.value = match
+      replyAddresses.value = []
+    } else if (!fromOptions.find((f) => f.value === fromEmail.value)) {
       fromEmail.value = fromOptions.length ? fromOptions[0].value : ''
     }
   },
@@ -344,6 +350,7 @@ defineExpose({
   cc,
   bcc,
   fromEmail,
+  replyAddresses,
   toEmails,
   ccEmails,
   bccEmails,
